2c7df14034dd55505450cd653e013b2999e84bc9
James Barrett <jabarret@microsoft.com>
2017-03-14 21:56:13 -0700

    vim-arrows instead of HHKB setup with the left FN

--- keyboards/tada68/keymaps/hhkb/keymap.c
+++ keyboards/tada68/keymaps/hhkb/keymap.c
@@ -6,6 +6,7 @@
 // entirely and just use numbers.
 #define BASE 0
 #define FL_HOLD 1
+#define FL_HOLD_LEFT 1
 #define FL_LOCK 2
 
 #define _______ KC_TRNS
@@ -29,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSPC,KC_PGUP, \
   KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_ENT,         KC_PGDN, \
   KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,           KC_UP,  MO(FL_HOLD),\
-  MO(FL_HOLD), KC_LGUI,KC_LALT,               KC_SPC,                         KC_RALT,KC_RGUI ,MO(FL_HOLD), KC_LEFT,KC_DOWN, KC_RGHT),
+  MO(FL_HOLD_LEFT), KC_LGUI,KC_LALT,               KC_SPC,                         KC_RALT,KC_RGUI ,MO(FL_HOLD), KC_LEFT,KC_DOWN, KC_RGHT),
 
   /* Keymap FL_HOLD: Primary Function Layer
    * ,----------------------------------------------------------------.
@@ -49,6 +50,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_CAPS,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_PAUS,KC_UP  ,_______, KC_DEL, KC_HOME, \
   _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,_______,_______,_______,KC_HOME,KC_PGUP,KC_LEFT,KC_RGHT, _______,KC_END, \
   _______,BL_DEC ,BL_INC, BL_TOGG,_______,_______,_______,_______,KC_END,KC_PGDN,KC_DOWN,_______,_______,_______, \
+  _______,_______,_______,                 _______,               _______,_______,_______,_______,_______, _______),
+
+  /* Keymap FL_HOLD_LEFT: Left-fn variant of primary function later, vim arrows intead of HHKB.
+   * ,----------------------------------------------------------------.
+   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|   INS |LFN2|
+   * |----------------------------------------------------------------|
+   * | CAPS|   |   |   |   |   |   |   |   |   |   |   |   | Del |Hom |
+   * |----------------------------------------------------------------|
+   * |      |VU-|VU+|MUT|   |   | < | v | ^ | ->|   |   |        |End |
+   * |----------------------------------------------------------------|
+   * |        |BL-|BL+|BL |   |   |   |   |   |   |   |      |   |    |
+   * |----------------------------------------------------------------|
+   * |    |    |    |                       |   |   |    |   |   |    |
+   * `----------------------------------------------------------------'
+   */
+[FL_HOLD_LEFT] = KEYMAP_ANSI(
+  _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS ,TO(FL_LOCK),  \
+  KC_CAPS,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, KC_DEL, KC_HOME, \
+  _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,_______, _______,KC_END, \
+  _______,BL_DEC ,BL_INC, BL_TOGG,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
   _______,_______,_______,                 _______,               _______,_______,_______,_______,_______, _______),
 
   /* Keymap FL_LOCK: Lockable secondary layer
